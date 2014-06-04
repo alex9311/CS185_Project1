9db170362ae560cbccc1018456a67a67061e8b14
MooCowofDom <moocowofdom@gmail.com>
2014-06-04 12:55:45 -0700

    actual initial commit?

---
+++ SeatRacing/src/edu/ucsb/cs/cs185/seatracing/BoatsetCreateActivity.java
@@ -0,0 +1,15 @@
+package edu.ucsb.cs.cs185.seatracing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BoatsetCreateActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_boatset_create);
+	}
+}
