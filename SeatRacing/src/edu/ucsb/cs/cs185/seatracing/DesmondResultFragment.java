736146b2586882166181156c90da0965b38ffd5c
alex9311 <alex.simes29@gmail.com>
2014-06-10 22:20:04 -0700

    Results stuff, this time for real

---
+++ SeatRacing/src/edu/ucsb/cs/cs185/seatracing/DesmondResultFragment.java
@@ -0,0 +1,19 @@
+package edu.ucsb.cs.cs185.seatracing;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DesmondResultFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.desmond_result_fragment, container, false);
+
+        return rootView;
+    }
+}
---
+++ SeatRacing/src/edu/ucsb/cs/cs185/seatracing/ExtendedResultFragment.java
@@ -0,0 +1,19 @@
+package edu.ucsb.cs.cs185.seatracing;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ExtendedResultFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.extended_result_fragment, container, false);
+
+        return rootView;
+    }
+}
