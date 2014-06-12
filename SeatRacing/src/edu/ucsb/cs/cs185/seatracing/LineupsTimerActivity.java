654f17f0d7a89528c9f71f62a9a29b616ead22d2
Evan West <e_west@umail.ucsb.edu>
2014-06-11 18:39:25 -0700

    fixed result bundling

--- SeatRacing/src/edu/ucsb/cs/cs185/seatracing/LineupsTimerActivity.java
+++ SeatRacing/src/edu/ucsb/cs/cs185/seatracing/LineupsTimerActivity.java
@@ -220,10 +220,9 @@ public class LineupsTimerActivity extends FragmentActivity implements AddNewSetL
 	}
 
 	private void switchToResultsActivity(Round round){
-
 	    Intent intent = new Intent(this, ResultsActivity.class);
 	    Bundle round_bundle = new Bundle();
-	    round.writeToBundle(1, round_bundle);
+	    round.writeResultsToBundle(round_bundle);
 	    intent.putExtra("name", round_bundle);
 	    startActivity(intent);
 	}
